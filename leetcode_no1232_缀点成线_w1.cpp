c6daad8f187cb37252c8a40453b890dabfeb7be9
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-01-17 21:56:52 +0800

    Add files via upload

---
+++ leetcode_no1232_缀点成线_w1.cpp
@@ -0,0 +1,91 @@
+﻿/* ------------------------------------------------------------------|
+在一个 XY 坐标系中有一些点，我们用数组 coordinates 来分别记录它们的坐标，其中 coordinates[i] = [x, y] 表示横坐标为 x、纵坐标为 y 的点。
+
+请你来判断，这些点是否在该坐标系中属于同一条直线上，是则返回 true，否则请返回 false。
+
+ 
+
+示例 1：
+
+
+
+输入：coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+输出：true
+示例 2：
+
+
+
+输入：coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+输出：false
+ 
+
+提示：
+
+2 <= coordinates.length <= 1000
+coordinates[i].length == 2
+-10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+coordinates 中不含重复的点
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/check-if-it-is-a-straight-line
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*
+*
+*	执行用时：8 ms, 在所有 C++ 提交中击败了99.57%的用户
+*	内存消耗：9.8 MB, 在所有 C++ 提交中击败了99.12%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+bool checkStraightLine(vector<vector<int>>& coordinates) {
+    int A = coordinates[1][0] - coordinates[0][0];
+    int B = -(coordinates[1][1] - coordinates[0][1]);
+    for (int i = 2; i < coordinates.size(); i++) {
+        int x = coordinates[i][0] - coordinates[0][0];
+        int y = coordinates[i][1] - coordinates[0][1];
+        if (A * y + B * x != 0) {
+            return false;
+        }
+    }
+    return true;
+}
